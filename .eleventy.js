8ae8f20033aaf2f8924fa452d5e8336f361dc0ef
Peter deHaan <peter@deseloper.com>
2024-02-03 10:13:48 -0800

    FIRST BLOOD

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+/**
+ * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
+ * @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
+ */
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      output: "www",
+      includes: "_includes"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk", // Not sure this is valid, per https://www.11ty.dev/docs/config/.
+  };
+};
